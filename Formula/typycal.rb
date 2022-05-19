cda2a7249c68137ecad10009d349c86a49a1cf17
Sebastian Rollen <seb@rollen.io>
2022-05-19 17:49:29 -0400

    v0.2.0

--- Formula/typycal.rb
+++ Formula/typycal.rb
@@ -1,9 +1,9 @@
 class Typycal < Formula
   desc 'A lightweight utility for typing and calculating stats about your typing speed.'
   homepage 'https://github.com/SebRollen/typycal'
-  url 'https://github.com/SebRollen/typycal/releases/download/0.1.0/typycal-mac.tar.gz'
-  sha256 'd925c56186f535389fc7a8b8e7f7f06e2c00c8a51fb4a39ddaf4cc4d245dde05'
-  version '0.1.0'
+  url 'https://github.com/SebRollen/typycal/releases/download/0.2.0/typycal-mac.tar.gz'
+  sha256 '0ade80371ed279a517b6ea53e693be5816ffa6d77c190b616fa16f21bea549c6'
+  version '0.2.0'
 
   def install
     bin.install 'typycal'
